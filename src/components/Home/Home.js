31dea5114cc763b1b328d64d44c3b3da2bea7e46
Himanshu <himanshurajput6654@gmail.com>
2023-12-30 23:30:36 +0530

    Adding Home features

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,29 @@
-import React from "react"
+import React from "react";
+import "./Home.css";
+import CowImage1 from "../../images/CowImage1.jpg";
 
-const Home = () =>{
-    return (
-        <>
-            Home
-        </>
-    )
-}
+const Home = () => {
+  return (
+    <div className="Home-Container">
+      <div className="imageContainer">
+        <img src={CowImage1} alt="cow" />
+      </div>
+      <div className="home-section-2">
+        <div className="section2-desc">
+          <div className="actual-desc"> 
+            Our mission is to save and enhance
+          the lives of cows, ensuring their well-being and promoting a
+          sustainable and ethical approach to cattle management.
+          We provide a nurturing environment, veterinary
+          services, and nutritional support. 
+          </div>
+        </div>
+        <div className="section2-smallDesc">
+            <div className="smallTalk-Desc">Take Action! Help us achieve this noble mission.</div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-
-export default Home
+export default Home;
