dff8022ac3b8cfca0d631c8703cc28ac10daeb97
Himanshu <himanshurajput6654@gmail.com>
2023-12-31 14:42:58 +0530

    Adds a section on home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,10 @@
 import React from "react";
 import "./Home.css";
 import CowImage1 from "../../images/CowImage1.jpg";
+import donationIcon from "../../images/Donation_icon.png";
+import adoptIcon from "../../images/Adopt_icon.png";
+import sponsorIcon from "../../images/Sponsor_icon.png";
+
 
 const Home = () => {
   return (
@@ -20,6 +24,47 @@ const Home = () => {
         </div>
         <div className="section2-smallDesc">
             <div className="smallTalk-Desc">Take Action! Help us achieve this noble mission.</div>
+        </div>
+      </div>
+      <div className="home-section-3">
+        <div className="home-section-outercard">
+          <div className="home-section-innercard">
+            <div className="card-icon">
+              <div className="inner-card-icon">
+                <img src={donationIcon} alt=""/>
+              </div>
+            </div>
+            <div className="card-heading"> Make Donation </div>
+            <div className="card-subheading"> Help us by making a donation </div>
+            <div className="card-desc">Donate generously so we can take care of our animal friends through our various initiatives and rescues.</div>
+            <div className="card-button"><button> Donate now </button></div>
+          </div>
+        </div>
+        <div className="home-section-outercard">
+          <div className="home-section-innercard">
+            <div className="card-icon">
+              <div className="inner-card-icon">
+                <img src={sponsorIcon} alt=""/>
+              </div>
+            </div>
+            <div className="card-heading"> Sponsor </div>
+            <div className="card-subheading"> Sponsor a cow at our shelter </div>
+            <div className="card-desc">The sponsorship will go towards the care of the animal and regular updates for the same will be provided to you.</div>
+            <div className="card-button"><button> Sponsor now </button></div>
+          </div>
+        </div>
+        <div className="home-section-outercard">
+          <div className="home-section-innercard">
+            <div className="card-icon">
+              <div className="inner-card-icon">
+                <img src={adoptIcon} alt=""/>
+              </div>
+            </div>
+            <div className="card-heading"> Adopt </div>
+            <div className="card-subheading"> Take home a new family member </div>
+            <div className="card-desc">Nothing brings more joy and satisfaction than to provide a forever home to one of our little wards.</div>
+            <div className="card-button"><button> Adopt now </button></div>
+          </div>
         </div>
       </div>
     </div>
